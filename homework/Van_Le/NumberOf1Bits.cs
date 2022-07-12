e93111a1e79a859e21dd5fd66bdfca244e18c12a
diegovanne <dinhvanle.it@gmail.com>
2022-07-12 10:07:38 +0700

    range sum query commit

--- homework/Van_Le/NumberOf1Bits.cs
+++ homework/Van_Le/NumberOf1Bits.cs
@@ -1,6 +1,8 @@
 // for any number of n, we can do a bitwise AND of n and n-1, it will flip only 1 bit of n into 0.
 // as soon as the number of n becomes 0, there will no longer be 1 bit left. 
 // So just repeatedly flip 1 bit and add 1 to the sum 
+// space complexity: O(1)
+// runtime complexity: O(1), because it repeatedly flip just the same as the number of 1 bits of n (dramatically smaller than length of n)
 
 public class NumberOf1Bits
 {
