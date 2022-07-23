66294cde836f51d34fdb447070333731db4ea244
Truong <tadashii1417@gmail.com>
2022-07-23 17:10:48 +0700

    Fix 238_Product_of_array_except_self not using devision

--- homework/Truong_Duong/lesson_3/238_Product_of_array_except_self.js
+++ homework/Truong_Duong/lesson_3/238_Product_of_array_except_self.js
@@ -4,28 +4,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var productExceptSelf = function(nums) {
-    let zeroCount = 0, products =  1, zeroIndex = -1;
-    const ans = new Array(nums.length).fill(0);
+var productExceptSelf = function (nums) {
+    const size = nums.length;
+    const ans = new Array(size).fill(1);
+    const prefixLeft = new Array(size).fill(1);
+    const prefixRight = new Array(size).fill(1);
 
-    for (let i = 0; i < nums.length; i++) {
-        const element = nums[i];
-        if(element === 0) {
-            zeroCount += 1;
-            zeroIndex = i;
-        } else {
-            products *= element;
-        }
+    for (let i = 1; i < size; i++) {
+        prefixLeft[i] = prefixLeft[i - 1] * nums[i - 1]
+    }
+    for (let i = size - 2; i >= 0; i--) {
+        prefixRight[i] = prefixRight[i + 1] * nums[ i + 1]
+    }
+    for (let i = 0; i < size; i++) {
+        ans[i] = prefixLeft[i] * prefixRight[i];
     }
 
-    if (zeroCount >= 2) return ans;
-    if (zeroCount === 1) {
-        ans[zeroIndex] = products;
-        return ans;
-    }
-
-    for(let i = 0; i < nums.length; i++) {
-        ans[i] = products/nums[i];
-    }
     return ans;
 };
