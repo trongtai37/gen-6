d1fe3c5bd64bb2fb6f5526bf9829083a5c8d48f7
Luong Viet Hai <hailv77@ghtk.co>
2022-07-24 15:49:07 +0700

    rechange to merge sort insteed build in function

--- homework/btvn-2/KClosestPoints.php
+++ homework/btvn-2/KClosestPoints.php
@@ -14,7 +14,7 @@ class KClosestPoints {
             $sortArray[] = $area;
         }
 
-        sort($sortArray);
+        $sortArray = $this->mergeSort($sortArray);
         $result = [];
 
         foreach ($points as $final) {
@@ -25,8 +25,71 @@ class KClosestPoints {
 
         return $result;
     }
+
+    public function mergeSort($array)
+    {
+        if (count($array) <= 1) {
+            return $array;
+        }
+
+        $left =  array();
+        $right = array();
+
+        $middle = (int) ( count($array)/2 );
+
+        for ($i = 0; $i < $middle; $i ++)
+        {
+            $left[] = $array[$i];
+        }
+
+        for ($i = $middle; $i < count($array); $i ++)
+        {
+            $right[] = $array[$i];
+        }
+
+
+        // Merge sort left & right
+        $this->mergeSort($left);
+        $this->mergeSort($right);
+
+        return $this->merge($left, $right);
+    }
+
+
+    public function merge($left, $right)
+    {
+        $result = [];
+
+        $i = 0;
+        $j = 0;
+
+        while($i < count($left) && $j < count($right)) {
+            if ($left[$i] < $right[$j]) {
+                $result[] = $left[$i];
+
+                $i++;
+            } else {
+                $result[] = $right[$j];
+                $j++;
+            }
+        }
+
+        while ($i < count($left)) {
+            $result[] = $left[$i];
+            $i ++;
+        }
+
+        while ($j < count($right)) {
+            $result[] = $right[$i];
+            $i ++;
+        }
+
+        return $result;
+    }
 }
 
 $test = new KClosestPoints;
+
+
 
 var_dump($test->kClosest([[1,3],[-2,2]],1));
