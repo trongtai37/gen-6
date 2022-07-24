c40e8df5b7b5c6469ebe1bbf7d3a7978bd57b016
Luong Viet Hai <hailv77@ghtk.co>
2022-07-24 16:17:47 +0700

    debug merge sort

--- homework/btvn-2/KClosestPoints.php
+++ homework/btvn-2/KClosestPoints.php
@@ -13,8 +13,8 @@ class KClosestPoints {
             $area = pow(abs($singlePoint[0]), 2) + pow(abs($singlePoint[1]), 2);
             $sortArray[] = $area;
         }
-
         $sortArray = $this->mergeSort($sortArray);
+
         $result = [];
 
         foreach ($points as $final) {
@@ -26,65 +26,49 @@ class KClosestPoints {
         return $result;
     }
 
-    public function mergeSort($array)
+    function mergeSort($Array)
     {
-        if (count($array) <= 1) {
-            return $array;
+        $len = count($Array);
+        if($len==1){
+            return $Array;
         }
-
-        $left =  array();
-        $right = array();
-
-        $middle = (int) ( count($array)/2 );
-
-        for ($i = 0; $i < $middle; $i ++)
-        {
-            $left[] = $array[$i];
-        }
-
-        for ($i = $middle; $i < count($array); $i ++)
-        {
-            $right[] = $array[$i];
-        }
-
-
-        // Merge sort left & right
-        $this->mergeSort($left);
-        $this->mergeSort($right);
-
+        $mid = (int)$len / 2;
+        $left = $this->mergeSort(array_slice($Array, 0, $mid));
+        $right = $this->mergeSort(array_slice($Array, $mid));
         return $this->merge($left, $right);
     }
 
-
-    public function merge($left, $right)
+    function merge($left, $right)
     {
-        $result = [];
+        $res = array();
 
-        $i = 0;
-        $j = 0;
-
-        while($i < count($left) && $j < count($right)) {
-            if ($left[$i] < $right[$j]) {
-                $result[] = $left[$i];
-
-                $i++;
-            } else {
-                $result[] = $right[$j];
-                $j++;
+        while (count($left) > 0 && count($right) > 0)
+        {
+            if($left[0] > $right[0])
+            {
+                $res[] = $right[0];
+                $right = array_slice($right , 1);
+            }
+            else
+            {
+                $res[] = $left[0];
+                $left = array_slice($left, 1);
             }
         }
 
-        while ($i < count($left)) {
-            $result[] = $left[$i];
-            $i ++;
+        while (count($left) > 0)
+        {
+            $res[] = $left[0];
+            $left = array_slice($left, 1);
         }
 
-        while ($j < count($right)) {
-            $result[] = $right[$i];
-            $i ++;
+        while (count($right) > 0)
+        {
+            $res[] = $right[0];
+            $right = array_slice($right, 1);
         }
 
-        return $result;
+        return $res;
     }
 }
 
@@ -92,4 +76,6 @@ $test = new KClosestPoints;
 
 
 
-var_dump($test->kClosest([[1,3],[-2,2]],1));
+//var_dump($test->kClosest([[1,3],[-2,2]],1));
+var_dump($test->kClosest([[1,3],[-2,2],[1,1]],1));
+//var_dump($test->kClosest([[1,3],[-2,2],[-1,-10],[-2,-9],[-1,0]],1));
