25f6b9ab54a233965ff67b24d72ad23af50309bc
ThinhNgVhust <45223680+ThinhNgVhust@users.noreply.github.com>
2022-07-27 00:36:50 +0900

    Design Partten, Note for some Python OOP, SOLID-> 2morrow

--- homework/thinhbka/lesson-4-hashing/0_Subarray Sum Equals K.py
+++ homework/thinhbka/lesson-4-hashing/0_Subarray Sum Equals K.py
@@ -1,3 +1,38 @@
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        
+from __future__ import annotations
+from abc import ABC,abstractmethod
+
+
+class Pet(ABC):
+    @abstractmethod
+    def say():
+        pass
+
+class Cat(Pet):
+    def say():
+        print("Cat goes: meowwww")
+
+class Dog(Pet):
+    def say():
+        print("Dog goes:Go go!")
+
+class Fish(Pet):
+    def say():
+        print("Fish is silent")
+class Chicken(Pet):
+    def say():
+        print("O O O")
+class PetCreator:
+    def __init__(self) -> None:
+        self.petMaker = { "Cat":Cat,"Dog":Dog,"Fish":Fish,"Chicken":Chicken }
+    
+    def create(self,type):
+        return self.petMaker[type]
+
+petCreator = PetCreator()
+myPet = petCreator.create("Cat")
+myPet.say()
+animals = ["Dog","Fish","Cat","Chicken"]
+for e in animals:
+    petCreator.create(e).say()
+
+    
