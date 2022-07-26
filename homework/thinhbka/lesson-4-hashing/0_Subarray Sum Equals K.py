38f53dac25e2c6371b81432d7796a20bcba55cae
ThinhNgV <nguyen-van-thinh@jcm-hq.co.jp>
2022-07-26 16:55:24 +0900

    aaa

--- homework/thinhbka/lesson-4-hashing/0_Subarray Sum Equals K.py
+++ homework/thinhbka/lesson-4-hashing/0_Subarray Sum Equals K.py
@@ -1,3 +1,18 @@
-class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        
+class Person:
+    def __init__(self,name) -> None:
+        self.name = name
+    
+class Life:
+    def __init__(self) -> None:
+        self.me = Person("Thinh")
+    def life(self):
+        while self.me:
+            while self.me.feelLike("ImposterSyndrome") is True:
+                self.me.talkWithOthers()
+                self.me.stopComparing()
+                self.me.stopDoubtingMySelf()
+                self.me.stopThinking()
+                self.me.listenToMusic()
+                self.me.listenToPodcast()
+        self.me.believeInMySelf()
+        self.me.keepMoving()
