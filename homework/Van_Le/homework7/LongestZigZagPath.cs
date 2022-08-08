23a018336653487895f005099a918353e60da9b8
diegovanne <dinhvanle.it@gmail.com>
2022-08-09 00:19:44 +0700

    update hw7

--- homework/Van_Le/homework7/LongestZigZagPath.cs
+++ homework/Van_Le/homework7/LongestZigZagPath.cs
@@ -1,45 +1,48 @@
-public class LongestZigZagPath {
-    private int Helper(TreeNode node, bool toLeft, IDictionary<(TreeNode node, bool left), int> cache, ref int res)
-    {
-        if (node == null)
-        {
-            return 0;
-        }
-
-        (TreeNode node, bool left) key = (node, toLeft);
-        if (cache.ContainsKey(key))
-        {
-            return cache[key];
-        }
-
-        int l = 1;
-
-        int leftLeft = Helper(node.left, true, cache, ref res);
-        int leftRight = Helper(node.left, false, cache, ref res);
-        int rightLeft = Helper(node.right, true, cache, ref res);
-        int rightRight = Helper(node.right, false, cache, ref res);
-
-        if (toLeft)
-        {
-            l += leftRight;
-        }
-        else
-        {
-            l += rightLeft;
-        }
-
-        res = Math.Max(res, l);
-
-        cache[key] = l;
-        return l;
-    }
-
+// explaination: https://bit.ly/3JEJkJU
+public class LongestZigZagPath
+{
+    private enum Direction { Left, Right };
     public int LongestZigZag(TreeNode root)
     {
-        IDictionary<(TreeNode node, bool left), int> cache = new Dictionary<(TreeNode node, bool left), int>();
-        int res = 0;
-        Helper(root, false, cache, ref res);
-        Helper(root, true, cache, ref res);
-        return res - 1;
+        int toLeft = LongestZigZag(root.left, Direction.Left, 0);
+        int toRight = LongestZigZag(root.right, Direction.Right, 0);
+        return Math.Max(
+            toLeft,
+            toRight
+        );
+    }
+
+    private int LongestZigZag(TreeNode node, Direction direction, int length)
+    {
+        if (node == null)
+            return length;
+        if (direction == Direction.Left)
+        {
+            int leftToRight = LongestZigZag(node.right, Direction.Right, length + 1);
+            int leftToLeft = LongestZigZag(node.left, Direction.Left, 0);
+            return Math.Max(
+                leftToRight,
+                leftToLeft
+            );
+        }
+        int rightToLeft = LongestZigZag(node.left, Direction.Left, length + 1);
+        int rightToRight = LongestZigZag(node.right, Direction.Right, 0);
+        return Math.Max(
+            rightToRight,
+            rightToLeft
+        );
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
