ccf50761e0e97a3d2c13252467681a93d9134aa4
ThinhNgV <nguyen-van-thinh@jcm-hq.co.jp>
2022-08-29 19:22:13 +0900

    Rakuten

--- homework/thinhbka/lesson-7-twopointer-heap/76. Minimum Window Substring.py
+++ homework/thinhbka/lesson-7-twopointer-heap/76. Minimum Window Substring.py
@@ -1,42 +1,33 @@
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        counter_t = Counter(t) #O(t)
-        counter_s = {}
-        right = 0
-        left = 0
-        ans = []
-        while right < len(s):#O(s)
-            number_distint = 0
-            char = s[right]
-            #add to hashmap
-            if char in counter_t:
-                if char not in counter_s:
-                    counter_s[char] = 0
-                counter_s[char] +=1
+        if t == "":
+            return ""
+        countT,window = Counter(t), {}
+        have, need = 0,len(countT)
+        res, resLen  = [-1,-1],float("infinity")
+        l,r = 0,0
+        for r in range(len(s)):
+            c = s[r]
+            window[c]  = window.get(c,0) + 1
 
-                #check for corresponding 
-                for c in counter_t:#O(52) because in worst case the are 52 characters in counter_t,btw how to optimal this part?
-                    if c in counter_t and c in counter_s and counter_s[c]>=counter_t[c]:
-                        number_distint+=1
-                if number_distint == len(counter_t):# if find a possible answer
-                    while number_distint == len(counter_t):
-                        if s[left] in counter_s:
-                            counter_s[s[left]]-=1
-                            if counter_s[s[left]] < counter_t[s[left]]:
-                                number_distint-=1
-                                if counter_s[s[left]] == 0:
-                                    del counter_s[s[left]]
-                        left+=1
-                    if len(ans) == 0 or ans[1]-ans[0] > right-left+1:
-                        ans = [left-1,right]
-                        print(s[ans[0]:ans[1]+1])
-                    
-            right+=1
-        if ans:
-            # print(ans)
-            return s[ans[0]:ans[1]+1]
-        return ""
+            if c in countT and window[c] == countT[c]:
+                have+=1
+
+            while have == need:
+                #update result
+                if(r-l+1) < resLen:
+                    res = [l,r]
+                    resLen = r-l+1
+                window[s[l]]-=1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have-=1
+                l+=1
+            r+=1
+        l,r  = res
+        ans =  s[l:r+1] if resLen!=float("infinity") else "" 
+        print(ans)
+        return ans
 Solution().minWindow("acbbaca","aba")
 Solution().minWindow("aa","aa")
 Solution().minWindow("ADOBECODEBANC","ABC")
